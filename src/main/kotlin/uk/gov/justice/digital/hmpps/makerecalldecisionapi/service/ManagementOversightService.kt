734aecd89204c434b930b39981fbce6b2854cbea
Bill Sclater <bill.sclater@digital.justice.gov.uk>
2023-04-24 12:42:35 +0100

    Revert "mrd-876_domain-events: correcttion to management-oversight service"
    This reverts commit b94b0ad9be26d8ba4cd79f5fb75c3da95cb4eb8f.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/ManagementOversightService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/service/ManagementOversightService.kt
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.makerecalldecisions.ManagementOversightResponse
-import uk.gov.justice.digital.hmpps.makerecalldecisionapi.jpa.entity.Status
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.jpa.repository.RecommendationRepository
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.util.DateTimeHelper.Helper.localDateTimeFromString
 import uk.gov.justice.digital.hmpps.makerecalldecisionapi.util.DateTimeHelper.Helper.splitDateTime
@@ -26,19 +25,17 @@ internal class ManagementOversightService(
   suspend fun getManagementOversightResponse(
     crn: String
   ): ResponseEntity<ManagementOversightResponse> {
-    val recommendations = crn?.let { recommendationRepository.findByCrnAndStatus(it, listOf(Status.DRAFT.name, Status.RECALL_CONSIDERED.name)) }
-    // recommendationRepository.findByCrn(crn)
+    val recommendations = recommendationRepository.findByCrn(crn)
     Collections.sort(recommendations)
-    val recommendation = recommendations?.first()
-    val dateTime = splitDateTime(localDateTimeFromString(recommendations?.get(0)?.data?.managerRecallDecision?.createdDate))
-    val readableNameOfUser = recommendation?.data?.managerRecallDecision?.createdBy
+    val dateTime = splitDateTime(localDateTimeFromString(recommendations[0].data.managerRecallDecision?.createdDate))
+    val readableNameOfUser = recommendations[0].data.managerRecallDecision?.createdBy
     return ResponseEntity(
       ManagementOversightResponse(
-        sensitive = recommendation?.data?.sensitive ?: false,
+        sensitive = recommendations[0].data.sensitive ?: false,
         notes = "Comment added by $readableNameOfUser on ${dateTime.first} at ${dateTime.second}: " +
           "$readableNameOfUser entered the following into the service 'Decide if someone should be recalled or not': " +
-          "${recommendation?.data?.managerRecallDecision?.selected?.details} " +
-          "View the case summary: $mrdUrl/cases/${recommendation?.data?.crn}/overview"
+          "${recommendations[0].data.managerRecallDecision?.selected?.details} " +
+          "View the case summary: $mrdUrl/cases/${recommendations[0].data.crn}/overview"
       ),
       HttpStatus.OK
     )
