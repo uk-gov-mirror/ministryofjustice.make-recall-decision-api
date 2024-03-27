dc276e99cc5cf7c0a5771c1363721d75082e3f4f
bills7281 <104494371+bills7281@users.noreply.github.com>
2024-03-27 10:06:59 +0000

    mrd-2459_bulk-delete-rework-disable-task: disabling bulk delete scheduled task (#964)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/MakeRecallDecisionApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/MakeRecallDecisionApi.kt
@@ -2,10 +2,10 @@ package uk.gov.justice.digital.hmpps.makerecalldecisionapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
+// import org.springframework.scheduling.annotation.EnableScheduling
 
+// @EnableScheduling
 @SpringBootApplication()
-@EnableScheduling
 class MakeRecallDecisionApi
 
 fun main(args: Array<String>) {
