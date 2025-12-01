0d2aba41cc7e5d36041c3a4c399c42a909187a5a
fincardy-moj <fin.cardy@justice.gov.uk>
2025-12-01 09:11:06 +0000

    MRD-2997 update plugin to fix CVE issue (#1245)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.1.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.2.0"
   kotlin("jvm") version "2.2.20"
   id("org.unbroken-dome.test-sets") version "4.1.0"
   id("jacoco")
