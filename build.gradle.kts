2cb5ca0432f3148b9c45b9a91230e9bf77b2ef4e
Pablo Mascaraque Glenn <pablo.glenn@digital.justice.gov.uk>
2025-12-23 09:50:00 +0000

    Upgrade HMPPS plug-in version (#1250)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.2.0"
-  kotlin("jvm") version "2.2.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.3.0"
+  kotlin("jvm") version "2.3.0"
   id("org.unbroken-dome.test-sets") version "4.1.0"
   id("jacoco")
-  kotlin("plugin.jpa") version "2.2.20"
+  kotlin("plugin.jpa") version "2.3.0"
   id("org.sonarqube") version "6.2.0.5505"
-  kotlin("plugin.spring") version "2.2.20"
-  kotlin("plugin.serialization") version "2.2.20"
+  kotlin("plugin.spring") version "2.3.0"
+  kotlin("plugin.serialization") version "2.3.0"
 }
 
 jacoco.toolVersion = "0.8.11"
