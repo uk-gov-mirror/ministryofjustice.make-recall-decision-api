1ceec2ce197b5b31a4f0a3f0c1d3e4f8b67fbae9
sureshmoj <suresh.mandalapu@justice.gov.uk>
2026-02-11 10:14:08 +0000

    Update RiskScoreType.kt (#1258)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/oasysarnapi/RiskScoreType.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/makerecalldecisionapi/domain/oasysarnapi/RiskScoreType.kt
@@ -1,11 +1,11 @@
 package uk.gov.justice.digital.hmpps.makerecalldecisionapi.domain.oasysarnapi
 
 enum class RiskScoreType(val printName: String) {
-  OSPDC("OSP/DC"),
-  OSPIIC("OSP/IIC"),
-  OSPC("OSP/C"),
-  OSPI("OSP/I"),
-  OGRS("OGRS"),
+  OSPDC("OSP-DC"),
+  OSPIIC("OSP-IIC"),
+  OSPC("OSP-C"),
+  OSPI("OSP-I"),
+  OGRS("OGRS3"),
   OGP("OGP"),
   OVP("OVP"),
   RSR("RSR"),
