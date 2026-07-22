dc93e40ba4b969b157c6effb1604cdf28ca67719
Bhanu Yandra <76459315+bhanugnosis@users.noreply.github.com>
2026-07-22 11:27:22 +0100

    CVE-2026-59084 and CVE-2026-59083 fixes (#1356)
    Signed-off-by: Bhanu Yandra <bhanu.yandra@justice.gov.uk>

--- build.gradle.kts
+++ build.gradle.kts
@@ -96,6 +96,21 @@ dependencies {
     }
   }
 
+  // hmpps-spring-boot plugin explicitly forcing the tomcat-embed-core version, so we can't override using constraints
+  implementation("org.apache.tomcat.embed:tomcat-embed-websocket") {
+    version {
+      strictly("11.0.24")
+    }
+    because("Address CVE-2026-59084 - can be removed once uk.gov.justice.hmpps.gradle-spring-boot to 11.0.x ")
+  }
+  // hmpps-spring-boot plugin explicitly forcing the tomcat-embed-core version, so we can't override using constraints
+  implementation("org.apache.tomcat.embed:tomcat-embed-core") {
+    version {
+      strictly("11.0.24")
+    }
+    because("Address CVE-2026-59084 - can be removed once uk.gov.justice.hmpps.gradle-spring-boot to 11.0.x ")
+  }
+
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.boot:spring-boot-webtestclient")
   testImplementation("org.springframework:spring-test")
